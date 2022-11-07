f8dd7f6f13a0d6aa5b39c8a1db7f26b9a1c24258
mrinmoy-iitg <mrin.mb@gmail.com>
2022-11-07 21:37:18 +0530

    Updated index, publications, experience, misc

--- js/core.js
+++ js/core.js
@@ -17,3 +17,30 @@ function show_abstract(id){
 		document.getElementById(id.split('_')[2]).style.display = "none";
 	}
 }
+function focus_segment(id){
+	if(id=="teaching_exp"){
+		document.getElementById("teaching_exp").style.display = "block";
+		document.getElementById("work_exp").style.display = "none";
+		document.getElementById("talks").style.display = "none";
+		document.getElementById("teaching_exp_false_button").style.backgroundColor = "rgba(230, 242, 189, 1)";
+		document.getElementById("work_exp_false_button").style.backgroundColor = "white";
+		document.getElementById("talks_false_button").style.backgroundColor = "white";
+	}
+	else if(id=="work_exp"){
+		document.getElementById("teaching_exp").style.display = "none";
+		document.getElementById("work_exp").style.display = "block";
+		document.getElementById("talks").style.display = "none";
+		document.getElementById("teaching_exp_false_button").style.backgroundColor = "white";
+		document.getElementById("work_exp_false_button").style.backgroundColor = "rgba(242, 222, 250, 1)";
+		document.getElementById("talks_false_button").style.backgroundColor = "white";
+	}
+	else if(id=="talks"){
+		document.getElementById("teaching_exp").style.display = "none";
+		document.getElementById("work_exp").style.display = "none";
+		document.getElementById("talks").style.display = "block";
+		document.getElementById("teaching_exp_false_button").style.backgroundColor = "white";
+		document.getElementById("work_exp_false_button").style.backgroundColor = "white";
+		document.getElementById("talks_false_button").style.backgroundColor = "rgba(237, 207, 180, 1)";
+	}
+}
+
